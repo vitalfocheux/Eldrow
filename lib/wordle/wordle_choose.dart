892ab6cb4c2637688cc913deefa8029253f7d6cb
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2024-11-11 17:38:55 +0100

    Ajout des boutons en directions des modes de jeux survie & duel

--- lib/wordle/wordle_choose.dart
+++ lib/wordle/wordle_choose.dart
@@ -1,5 +1,11 @@
+import 'dart:math';
+
 import 'package:Wordle/wordle/start_classic.dart';
+import 'package:Wordle/wordle/start_dual.dart';
+import 'package:Wordle/wordle/wordle_survival.dart';
 import 'package:flutter/material.dart';
+
+import '../word_list/word_lists.dart';
 
 class WordleChoose extends StatefulWidget {
 
@@ -34,13 +40,23 @@ class _WordleChooseState extends State<WordleChoose> {
                   },
                   child: const Text('Classic Wordle'),
               ),
-              const ElevatedButton(
-                  onPressed: null,
-                  child: Text('Survival Wordle'),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SurvivalWordle(title: 'Survival Wordle', maxAttemps: 9, dictionary: WordLists().frenchWords, language: 'fr')),
+                    );
+                  },
+                  child: const Text('Survival Wordle'),
               ),
-              const ElevatedButton(
-                onPressed: null,
-                child: Text("Dual Wordle"),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const StartDual()),
+                  );
+                },
+                child: const Text("Dual Wordle"),
               ),
             ],
           ),
